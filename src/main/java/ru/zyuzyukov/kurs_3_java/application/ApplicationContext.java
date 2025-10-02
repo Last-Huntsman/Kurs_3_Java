632f97a492d1b28504604834a767b7f1bee7cb20
Egor <zuzik17@yandex.ru>
2025-10-02 19:13:40 +0300

    feat(ApplicationContext): Added connectionManager for Repository

--- src/main/java/ru/zyuzyukov/kurs_3_java/application/ApplicationContext.java
+++ src/main/java/ru/zyuzyukov/kurs_3_java/application/ApplicationContext.java
@@ -4,34 +4,41 @@ import ru.zyuzyukov.kurs_3_java.db.entity.*;
 import ru.zyuzyukov.kurs_3_java.db.repositories.*;
 import ru.zyuzyukov.kurs_3_java.db.service.BaseService;
 import ru.zyuzyukov.kurs_3_java.dto.*;
+import ru.zyuzyukov.kurs_3_java.jdbc.ConnectionManager;
 import ru.zyuzyukov.kurs_3_java.mapper.*;
 
 public class ApplicationContext {
-    private  final EmployerRepository employerRepository = new EmployerRepository() ;
-    private final EmployerMapper employerMapper = new EmployerMapper() ;
-    private final EmploymentRepository employmentRepository = new EmploymentRepository() ;
-    private final EmploymentMapper employmentMapper = new EmploymentMapper() ;
-    private final SkillRepository skillRepository = new SkillRepository() ;
-    private final SkillMapper skillMapper = new SkillMapper() ;
-    private final VacancyRepository vacancyRepository = new VacancyRepository() ;
-    private final VacancyMapper vacancyMapper = new VacancyMapper() ;
-    private final WorkerRepository workerRepository = new WorkerRepository() ;
-    private final WorkerMapper workerMapper = new WorkerMapper() ;
+
+    private final ConnectionManager connectionManager = new ConnectionManager();
+    private final EmployerRepository employerRepository = new EmployerRepository(connectionManager);
+    private final EmployerMapper employerMapper = new EmployerMapper();
+    private final EmploymentRepository employmentRepository = new EmploymentRepository(connectionManager);
+    private final EmploymentMapper employmentMapper = new EmploymentMapper();
+    private final SkillRepository skillRepository = new SkillRepository(connectionManager);
+    private final SkillMapper skillMapper = new SkillMapper();
+    private final VacancyRepository vacancyRepository = new VacancyRepository(connectionManager);
+    private final VacancyMapper vacancyMapper = new VacancyMapper();
+    private final WorkerRepository workerRepository = new WorkerRepository(connectionManager);
+    private final WorkerMapper workerMapper = new WorkerMapper();
 
 
-    public BaseService<EmployerDto,Employer> getEmployerService() {
+    public BaseService<EmployerDto, Employer> getEmployerService() {
         return new BaseService<>(employerRepository, employerMapper);
     }
-    public BaseService<SkillDto,Skill> getSkillService() {
-        return new BaseService<>(skillRepository,skillMapper);
+
+    public BaseService<SkillDto, Skill> getSkillService() {
+        return new BaseService<>(skillRepository, skillMapper);
     }
+
     public BaseService<EmploymentDto, Employment> getEmploymentService() {
         return new BaseService<>(employmentRepository, employmentMapper);
     }
-    public BaseService<VacancyDto,Vacancy> getVacancyService() {
+
+    public BaseService<VacancyDto, Vacancy> getVacancyService() {
         return new BaseService<>(vacancyRepository, vacancyMapper);
     }
-    public BaseService<WorkerDto,Worker> getWorkerService() {
+
+    public BaseService<WorkerDto, Worker> getWorkerService() {
         return new BaseService<>(workerRepository, workerMapper);
     }
 }
